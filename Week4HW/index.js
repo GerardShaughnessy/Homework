3af346b00e0492b2c0c390aa25d8cea22564784c
Gerard Shaughnessy <igpshaughnessy@gmail.com>
2022-05-01 14:19:54 -0500

    homework almost done

---
+++ Week4HW/index.js
@@ -0,0 +1,5 @@
+const listElementOne = document.createElement("li");
+////
+listElementOne.textContent = "It is a construct to control students";
+document.querySelector("h3").appendChild(listElementOne);
+
