86ada2681a6c5724b278a40d97d54f5ffcf72337
Gerard Shaughnessy <igpshaughnessy@gmail.com>
2022-05-02 16:34:28 -0500

    finished homework

--- Week4HW/index.js
+++ Week4HW/index.js
@@ -1,5 +1,5 @@
 const listElementOne = document.createElement("li");
 ////
-listElementOne.textContent = "It is a construct to control students";
-document.querySelector("h3").appendChild(listElementOne);
+listElementOne.textContent = "It is a construct to control students!";
+document.querySelector(".list1").appendChild(listElementOne);
 
