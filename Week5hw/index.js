fc63b49a0b286af6568217db4be65aa207c07cfe
Gerard Shaughnessy <igpshaughnessy@gmail.com>
2022-05-08 14:43:39 -0500

    week 5 homeowrk almost done

---
+++ Week5hw/index.js
@@ -0,0 +1,14 @@
+// CSS Media Queries Practice
+// Section 2 of 4
+
+// Included in week 5 homework is the index.html file from a site about Andre the Giant.
+
+// In the styles.css file, fix the styling for the site, and create a media query that fixes the site to correctly display on the "iPhone 6/7/8" view in DevTools.
+
+// Focus on the following:
+
+// The #bio <ul> at the top of the page
+// <img> tags
+// <div> tags
+// links
+// TIP: you may want to copy-paste some already existing css rules into your media query to make sure you are overwriting the necessary rules.
